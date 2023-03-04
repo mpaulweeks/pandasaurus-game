5ab8cce1e266da2fc3669afa30713ab768802cba
mpaulweeks <mpaulweeks@gmail.com>
2023-03-03 20:58:30 -0500

    sorting and validating plays

--- src/lib/executor.ts
+++ src/lib/executor.ts
@@ -1,4 +1,22 @@
-import { CardData, GameState } from "./types"
+import { CardData, GameState, StackState } from "./types"
+
+function sortCards(arr: CardData[], ascending: boolean) {
+  function compare(a: CardData, b: CardData) {
+    if (a.value < b.value || a.value === b.value + 10) {
+      return -1;
+    }
+    return 1;
+  }
+  const out = arr.concat();
+  out.sort(compare);
+  return ascending ? out : out.reverse();
+}
+
+function canPlay(stack: StackState, card: CardData) {
+  const stackTop = stack.cards.slice(-1)[0];
+  const sorted = sortCards([stackTop, card], stack.ascending);
+  return sorted[0].cid === stackTop.cid;
+}
 
 export function executePlay(state: GameState, play: {
   sid: string;
@@ -19,13 +37,16 @@ export function executePlay(state: GameState, play: {
 
   // if no card found?
   if (card === undefined) {
-    // no-op
-    return state;
+    throw new Error('playing a card not in any hands: ' + play.cid);
   }
 
   out.board.forEach(stack => {
-    if (stack.sid === play.sid) {
-      stack.cards.push(card!);
+    if (card && stack.sid === play.sid) {
+      if (canPlay(stack, card)) {
+        stack.cards.push(card);
+      } else {
+        throw new Error('playing a card higher than the stack: ' + play.cid);
+      }
     }
   });
 
@@ -36,10 +57,20 @@ export function executePlays(state: GameState, play: {
   sid: string;
   cards: string[];
 }): GameState {
-  // todo sort cards with 10 skip in mind
+  const stack = state.board.filter(stack => stack.sid === play.sid)[0];
+  const cards = state.hands.map(hand => hand.cards.filter(c => play.cards.includes(c.cid))).flat();
+
+  if (!stack) {
+    throw new Error('stack not found: ' + play.sid);
+  }
+  if (cards.length !== play.cards.length) {
+    throw new Error('invalid batch plays');
+  }
+
+  const sorted = sortCards(cards, stack.ascending);
   let out = state;
-  for (const cid of play.cards) {
-    out = executePlay(out, { sid: play.sid, cid, });
+  for (const card of sorted) {
+    out = executePlay(out, { sid: stack.sid, cid: card.cid, });
   }
   return out;
 }
